57018a8400009b9ae63da4834ca3069e6cfcad24
YuJiaLiang0702 <ch993115@gmail.com>
2024-11-13 00:09:30 +0800

    Initial Commit

---
+++ TrueOrFalse/TrueOrFalseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrueOrFalseApp.swift
+//  TrueOrFalse
+//
+//  Created by Jesse Liang on 2024/11/13.
+//
+
+import SwiftUI
+
+@main
+struct TrueOrFalseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrueOrFalseTests/TrueOrFalseTests.swift
@@ -0,0 +1,16 @@
+//
+//  TrueOrFalseTests.swift
+//  TrueOrFalseTests
+//
+//  Created by Jesse Liang on 2024/11/13.
+//
+
+import Testing
+
+struct TrueOrFalseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
